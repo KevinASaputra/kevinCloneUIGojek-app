6baee90303ff6304188208267fd9b780966272af
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-09 02:22:00 +0700

    add search fitur

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import DiscountScreen from "./src/screens/discountScreen";
 import SaldoCard from "./src/components/saldoCard";
+import SearchBar from "./src/components/searchBar";
 
 export default function App() {
   return (
     <View style={styles.rootScreens}>
+      <SearchBar />
       <SaldoCard />
       <StatusBar style="auto" />
     </View>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 function searchBar() {
   return (
-    <View>
-      <TextInput></TextInput>
-      <Text>Test</Text>
+    <View style={styles.rootContainer}>
+      <View style={styles.inputContainer}>
+        <Ionicons
+          style={{ paddingStart: 10 }}
+          name="ios-search"
+          size={24}
+          color="#a7a7a7"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Find service, Food, or Place"
+        />
+      </View>
+      <FontAwesome
+        style={{ paddingStart: 25 }}
+        name="user-circle"
+        size={34}
+        color="white"
+      />
     </View>
   );
 }
 
 export default searchBar;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    paddingTop: 40,
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#007419",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: "70%",
+    height: 35,
+    alignItems: "center",
+    marginVertical: 20,
+    marginHorizontal: 20,
+    borderRadius: 30,
+    backgroundColor: "white",
+  },
+  textInput: {
+    paddingHorizontal: 10,
+  },
+});
