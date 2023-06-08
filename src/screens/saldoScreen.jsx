90fbd76df7a05fddfa74447073f5bea3b1e82d77
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-08 21:04:25 +0700

    rename

---
+++ src/screens/saldoScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, TouchableHighlight } from "react-native";
+
+function saldoScreens() {
+  return (
+    <View>
+      <TouchableHighlight></TouchableHighlight>
+    </View>
+  );
+}
+
+export default saldoScreens;
