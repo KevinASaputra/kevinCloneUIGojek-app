a56cb6e10fce33500655dd8234e40f33e3bace50
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-10 20:50:50 +0700

    metro config

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,19 @@
-const { getDefaultCOnfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
