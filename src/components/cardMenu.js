3afcb6b48e724475f9ee91031b16a7ad6b8f92d4
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-10 20:41:18 +0700

    bug

--- src/components/cardMenu.js
+++ src/components/cardMenu.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
-import IconChat from "../assets/icon_chat.svg";
+// import IconChat from "../assets/icon_chat.svg";
 
 function cardMenu() {
-  return (
-    <View>
-      <IconChat width={200} height={200} />
-    </View>
-  );
+  return <View>{/* <IconChat width={200} height={200} /> */}</View>;
 }
 
 export default cardMenu;
