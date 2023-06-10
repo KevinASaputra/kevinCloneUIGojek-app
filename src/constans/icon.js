31521405ad33773a97e3e092fb23cd2a285d1380
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-10 21:05:19 +0700

    import new icon

--- src/constans/icon.js
+++ src/constans/icon.js
@@ -2,5 +2,6 @@ import GorRIdeIcon from "../assets/icon_goride.svg";
 import ChatIcon from "../assets/icon_chat.svg";
 import GosendIcon from "../assets/icon_gosend.svg";
 import HomeIcon from "../assets/icon_home.svg";
+import ProfIcon from "../assets/icon_profile.svg";
 
 export { ChatIcon };
