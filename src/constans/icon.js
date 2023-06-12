15e93d07eeec52e773ac2292f1edae64dc07001f
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-12 20:03:46 +0700

    add icon in module icon.js

--- src/constans/icon.js
+++ src/constans/icon.js
@@ -1,7 +1,19 @@
-import GorRIdeIcon from "../assets/icon_goride.svg";
 import ChatIcon from "../assets/icon_chat.svg";
+import GoCarIcon from "../assets/icon_gocar.svg";
+import GoFoodIcon from "../assets/icon_gofood.svg";
+import GoRIdeIcon from "../assets/icon_goride.svg";
 import GosendIcon from "../assets/icon_gosend.svg";
 import HomeIcon from "../assets/icon_home.svg";
 import ProfIcon from "../assets/icon_profile.svg";
+import promosIcon from "../assets/icon_promos.svg";
 
-export { ChatIcon };
+export {
+  GoCarIcon,
+  GoFoodIcon,
+  ChatIcon,
+  GoRIdeIcon,
+  GosendIcon,
+  HomeIcon,
+  ProfIcon,
+  promosIcon,
+};
